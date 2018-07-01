702f2cc8d341032a662a61d6b0080dd74af0c188
xyp <rea.s.xyp@gmail.com>
2018-07-01 19:46:14 +0200

    Config.

--- config.py
+++ config.py
@@ -25,9 +25,9 @@ parser.add_argument('--nz', type=int, default=512)              # input dimensio
 parser.add_argument('--ngf', type=int, default=512)             # feature dimension of final layer of generator.
 parser.add_argument('--ndf', type=int, default=512)             # feature dimension of first layer of discriminator.
 parser.add_argument('--TICK', type=int, default=1000)           # 1 tick = 1000 images = (1000/batch_size) iter.
-parser.add_argument('--max_resl', type=int, default=8)          # 10-->1024, 9-->512, 8-->256
-parser.add_argument('--trns_tick', type=int, default=800)       # transition tick
-parser.add_argument('--stab_tick', type=int, default=800)       # stabilization tick
+parser.add_argument('--max_resl', type=int, default=10)          # 10-->1024, 9-->512, 8-->256
+parser.add_argument('--trns_tick', type=int, default=400)       # transition tick
+parser.add_argument('--stab_tick', type=int, default=400)       # stabilization tick
 
 
 ## network structure.
