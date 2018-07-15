d0449ae2489bb334004bcbea467472d1967949a5
Yanping <rea.s.xyp@gmail.com>
2018-07-15 20:19:11 +0200

    Fix plotting for last resolution.

--- extract_log.py
+++ extract_log.py
@@ -59,12 +59,12 @@ def main():
 
 			print("Finish parsing data for resolution", prev_resl)
 			plt.plot(d_loss)
-			plt.clf()
 			plt.savefig("d_loss_{}.png".format(prev_resl))
+			plt.clf()
 			print("D loss saved.")
 			plt.plot(g_loss)
-			plt.clf()
 			plt.savefig("g_loss_{}.png".format(prev_resl))
+			plt.clf()
 			print("G loss saved.")	
 
 	elif args.breakpoint:
