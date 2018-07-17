c91103ae24c469750f8a31083804e04ebe8706ef
Yanping <rea.s.xyp@gmail.com>
2018-07-17 22:48:22 +0200

    Modified gain for Kaiming intializer.

--- network.py
+++ network.py
@@ -85,6 +85,7 @@ class Generator(nn.Module):
         ndim = self.ngf
         if self.flag_norm_latent:
             layers.append(pixelwise_norm_layer())
+        layers = linear(layers, self.nz, self.nz*4*4, sig=False, wn=self.flag_wn)
         layers = deconv(layers, self.nz, ndim, 4, 1, 3, self.flag_leaky, self.flag_bn, self.flag_wn, self.flag_pixelwise)
         layers = deconv(layers, ndim, ndim, 3, 1, 1, self.flag_leaky, self.flag_bn, self.flag_wn, self.flag_pixelwise)
         return  nn.Sequential(*layers), ndim
