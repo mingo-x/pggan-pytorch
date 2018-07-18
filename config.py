416eb3d54156f03b6f3c8aefff4fa76f88de5473
Yanping <rea.s.xyp@gmail.com>
2018-07-18 15:15:05 +0200

    Fix repeat.

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ parser = argparse.ArgumentParser('PGGAN')
 
 ## general settings.
 parser.add_argument('--train_data_root', type=str, default='/srv/glusterfs/xieya/data')
-parser.add_argument('--random_seed', type=int, default=int(time.time()))
+parser.add_argument('--random_seed', type=int, default=1000)
 parser.add_argument('--n_gpu', type=int, default=1)             # for Multi-GPU training.
 parser.add_argument('--gen_ckpt', type=str, default='')
 parser.add_argument('--dis_ckpt', type=str, default='')  
--- todo.md
+++ todo.md
@@ -1,4 +1,3 @@
 1. Smoothed generator
 5. fp32
 3. Scale of fake loss smaller
-4. minibatch
